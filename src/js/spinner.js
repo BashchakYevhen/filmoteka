467a9f7cafad520bf9ea4c0e6f5bde3e3d222830
Yevhen Bashchak <evgeny221@gmail.com>
2022-10-03 10:40:00 +0200

    node- install

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,20 +1,20 @@
 export default class Spinner {
-    constructor({hidden = false}) {
-        this.refs = this.getRefs();
-        hidden && this.hide();
-    }
+  constructor({ hidden = false }) {
+    this.refs = this.getRefs();
+    hidden && this.hide();
+  }
 
-    getRefs() {
-        const refs = {};
-        refs.loader = document.querySelector('.loader');
-        return refs;
-    }
+  getRefs() {
+    const refs = {};
+    refs.loader = document.querySelector('.loader');
+    return refs;
+  }
 
-    show() {
-        this.refs.loader.classList.remove('is-hidden');
-    }
+  show() {
+    this.refs.loader.classList.remove('is-hidden');
+  }
 
-    hide() {
-        this.refs.loader.classList.add('is-hidden')
-    }
-};
+  hide() {
+    this.refs.loader.classList.add('is-hidden');
+  }
+}
